fd0b15db541acf261e9fe14dea59e4a42e16ad5f
Azharafridi <99399706+Azharafridi@users.noreply.github.com>
2024-12-11 09:11:39 +0500

    second push

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # end_to_end_object_detection
+
+
+## how to run :
+
+'''
+    bash
+conda create -n signlang python=3.8.0 -y
+'''
+'''
+    bash
+conda activate signlang
+'''
+
+'''
+    bash
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='signlag',
+    version='0.0.0',
+    author='Azhar afridi',
+    author_email= 'azharafrdi909@gmail.com',
+    packages=find_packages(),
+
+    install_requires = [],
+    
+    
+)
